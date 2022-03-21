9c7b22afe384896c615ce95b967be80a3ee65349
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 12:19:38 +0530

    Add files via upload

---
+++ python_variable.py
@@ -0,0 +1,11 @@
+# Assign values to the variables
+
+counter =      # An integer assignment
+miles =         # A floating point
+name =        # A string
+gender =      # A character
+
+print(counter)
+print(miles)
+print(name)
+print(gender)
